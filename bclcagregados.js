bdd20ae86b3b2ada2c921899da1db9ed50d9e446
varelavirginia78-png <varela.virginia78@gmail.com>
2026-08-03 02:58:41 -0300

    Add files via upload

---
+++ bclcagregados.js
@@ -0,0 +1,5 @@
+const agregadosCLC = {
+
+};
+
+window.agregadosCLC = agregadosCLC;
---
+++ bclcflores.js
@@ -0,0 +1,126 @@
+const floresCLC = {
+
+"Hortensia": {
+tipo: "flor",
+codigo: "20",
+simbolo: "Gratitud",
+
+principal: "La gratitud y el amor compartido florecen cuando cuidamos aquello que realmente tiene valor en nuestra vida.",
+
+secundaria: ""
+
+},
+
+
+"Coronita de novia": {
+tipo: "flor",
+codigo: "21",
+simbolo: "Inocencia y pureza",
+
+principal: "Recordá que lo más delicado también puede ser aquello que más perdura.",
+
+secundaria: ""
+
+},
+
+
+"Crespón": {
+tipo: "flor",
+codigo: "22",
+simbolo: "Renovación y nuevos ciclos",
+
+principal: "Todo llega en el momento indicado. Confiá en tus tiempos y en la belleza de cada etapa.",
+
+secundaria: ""
+
+},
+
+
+"Lantana": {
+tipo: "flor",
+codigo: "23",
+simbolo: "Adaptación, alegría y transformación",
+
+principal: "Recordá que la transformación también puede ser una forma de belleza.",
+
+secundaria: ""
+
+},
+
+
+"Hardenbergia": {
+tipo: "flor",
+codigo: "24",
+simbolo: "Conexión y caminos que se entrelazan",
+
+principal: "Tu fuerza interior encuentra la forma de crecer, incluso cuando el camino se vuelve desafiante.",
+
+secundaria: ""
+
+},
+
+
+"Lobelia": {
+tipo: "flor",
+codigo: "25",
+simbolo: "Buena fortuna",
+
+principal: "La buena fortuna acompaña tus pasos y abre espacio para nuevas oportunidades.",
+
+secundaria: ""
+
+},
+
+
+"Pensamiento": {
+tipo: "flor",
+codigo: "26",
+simbolo: "Recuerdo y cariño",
+
+principal: "Aquello que pensamos y sentimos con amor también forma parte de nuestra historia.",
+
+secundaria: ""
+
+},
+
+
+"Pétalo de rosa": {
+tipo: "flor",
+codigo: "27",
+simbolo: "Amor y delicadeza",
+
+principal: "Recordá que lo que hoy parece pequeño también guarda la posibilidad de convertirse en algo maravilloso.",
+
+secundaria: ""
+
+},
+
+
+
+"Margarita": {
+tipo: "flor",
+codigo: "28",
+simbolo: "Inocencia, alegría y nuevos comienzos",
+
+principal: "La luz aparece cuando aprendemos a mirar con más sencillez y gratitud aquello que la vida nos regala.",
+
+secundaria: ""
+
+},
+
+"Verbena": {
+tipo: "flor",
+codigo: "29",
+simbolo: "Protección, renovación, armonía.",
+
+principal: "Permitite soltar todo aquello que pesa. Dejá que encuentre su camino hacia la luz. Hacé espacio para la armonía y la serenidad que merecés.",
+
+secundaria: ""
+
+}
+
+
+};
+
+
+window.floresCLC = floresCLC;
